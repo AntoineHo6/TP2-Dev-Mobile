72be71413a06899b82256e903929e644a63bf8d7
Antoine Ho <34996565+AntoineHo6@users.noreply.github.com>
2019-11-15 21:18:53 -0500

    Created google login btn. Not fully functional yet

--- app/src/main/java/Presenter/LoginPage.java
+++ app/src/main/java/Presenter/LoginPage.java
@@ -1,18 +1,32 @@
 package Presenter;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.graphics.drawable.AnimationDrawable;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.dev.TP2_Mobile.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
+import Model.Constants.Constants;
 import View.LoginPageView;
+
+import static Model.Constants.Constants.*;
+
 
 public class LoginPage extends AppCompatActivity {
 
     LoginPageView loginPageView;
+
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,44 @@ public class LoginPage extends AppCompatActivity {
         setContentView(R.layout.login_page);
 
         loginPageView = new LoginPageView(this);
+
+        // google auth
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                //firebaseAuthWithGoogle(account);
+                // WE SHOULD USE THE DB WE HAVE?
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("GOOGLE: ", "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 }
