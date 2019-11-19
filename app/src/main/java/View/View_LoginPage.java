d0eda4b9321435f0e5082a32ef426181fb109cd5
Lourhmati <1739188@bdeb.qc.ca>
2019-11-19 13:10:50 -0500

    Debut metier etudiant

--- app/src/main/java/View/View_LoginPage.java
+++ app/src/main/java/View/View_LoginPage.java
@@ -1,6 +1,9 @@
 package View;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +11,22 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.dev.TP2_Mobile.R;
 
+import java.util.ArrayList;
+
 public class View_LoginPage {
 
     AppCompatActivity activity;
+    Button login;
 
     public View_LoginPage(AppCompatActivity activity) {
         this.activity = activity;
+        login = activity.findViewById(R.id.btnLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMetierEtudiant(0, "add");
+            }
+        });
 
         ConstraintLayout constraintLayout = activity.findViewById(R.id.loginpage_layout);
         AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
@@ -22,4 +35,12 @@ public class View_LoginPage {
         animationDrawable.start();
     }
 
+    private void openMetierEtudiant(int i, String add) {
+        Intent intent = new Intent(activity.getApplicationContext(), View_MetierEtudiant.class);
+        activity.startActivity(intent);
+
+    }
 }
+
+
+
